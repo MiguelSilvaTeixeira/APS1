ddabd69213e6c4d88c971776bef778fdc3dc5bc1
Miguel Teixeira <teixeira.miguel.trabalho@gmail.com>
2024-03-22 14:07:26 -0300

    .

--- src/assets/header.js
+++ src/assets/header.js
@@ -32,7 +32,7 @@ class Header extends HTMLElement {
                 
                 .navbar a {
                     position: relative;
-                    font-size: 16px;
+                    font-size: 18px;
                     color: #fff;
                     font-weight: 500;
                     text-decoration: none;
