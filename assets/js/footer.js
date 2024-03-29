31f7fc20d3c9e079b2f43bf44f8b5c8b63c33c62
Miguel Teixeira <teixeira.miguel.trabalho@gmail.com>
2024-03-29 03:56:04 -0300

    Update Footer
    Adicionado o botão de voltar ao topo.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,17 +6,25 @@ export class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
-                footer>p {
-                    display: block;
-                    width: 100%;
-                    padding: 20px;
-                    background-color: #3e853c00;
+                footer {
                     text-align: center;
+                    margin-bottom: 20px;
+                }        
+                footer>p {
+                    display: inline-block;
+                    vertical-align: middle;
                     font-size: 18px;
                     color: #fff;
+                }        
+                #footer-top {
+                    height: 30px;
+                    width: 30px;
+                    vertical-align: middle;
+                    margin-left: 10px;
                 }
             </style>
             <footer>
+                <a href="#"><img src="../assets/img/seta-para-cima.png" alt="Voltar ao topo" id="footer-top"></a>             
                 <p> Copyright &copy; 2024</p>
             </footer>
         `;
